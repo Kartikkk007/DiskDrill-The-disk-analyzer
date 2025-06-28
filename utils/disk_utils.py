d3a9a6f51cfe2062cd17f26e1a3844c195535a28
Kartikkk007 <126849570+Kartikkk007@users.noreply.github.com>
2025-06-28 21:36:41 +0530

    Update disk_utils.py

--- utils/disk_utils.py
+++ utils/disk_utils.py
@@ -1,1 +1,44 @@
 
+import os
+import shutil
+
+def get_folder_contents(path):
+    contents = []
+    try:
+        with os.scandir(path) as entries:
+            for entry in entries:
+                try:
+                    size = get_size(entry.path)
+                    contents.append({
+                        'name': entry.name,
+                        'path': entry.path,
+                        'size': size,
+                        'is_dir': entry.is_dir()
+                    })
+                except:
+                    continue
+    except:
+        pass
+    return contents
+
+def get_size(path):
+    total = 0
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+    for dirpath, dirnames, filenames in os.walk(path, topdown=True):
+        for f in filenames:
+            try:
+                total += os.path.getsize(os.path.join(dirpath, f))
+            except:
+                continue
+    return total
+
+def delete_path(path):
+    try:
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
+        return True
+    except:
+        return False
